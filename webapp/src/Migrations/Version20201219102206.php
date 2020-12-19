738133f2a59a8888298ba24ccfcd6f39d588a898
Tobias Werth <werth@cs.fau.de>
2020-12-19 11:26:39 +0100

    migration

---
+++ webapp/src/Migrations/Version20201219102206.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20201219102206 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE testcase_content ADD CONSTRAINT FK_50A5CCE2D360BB2B FOREIGN KEY (testcaseid) REFERENCES testcase (testcaseid) ON DELETE CASCADE');
+        $this->addSql('DROP INDEX filename ON executable_file');
+        $this->addSql('CREATE UNIQUE INDEX filename ON executable_file (immutable_execid, filename(190))');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP INDEX filename ON executable_file');
+        $this->addSql('CREATE UNIQUE INDEX filename ON executable_file (immutable_execid, filename(190))');
+        $this->addSql('ALTER TABLE testcase_content DROP FOREIGN KEY FK_50A5CCE2D360BB2B');
+    }
+}
