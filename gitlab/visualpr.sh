1e027765913c2b718eedaa5f5913fe81c154c0a4
MCJ Vasseur <14887731+mvr320@users.noreply.github.com>
2020-12-31 22:38:44 +0100

    Use Cutycapt in favor of firefox
    Firefox does not capture the flag on the main page on every run,
    this creates the problem that there are false visual changes reported.
    Cutycapt should still use a relatively normal rendering engine but has
    more options to set.
    
    (cherry picked from commit e74384f5ecdbd4b24b5a2e87af7c951a9ee52439)

--- gitlab/visualpr.sh
+++ gitlab/visualpr.sh
@@ -98,6 +98,8 @@ cd $DIR
 STORAGEDIR=screenshots$1
 mkdir -p $STORAGEDIR
 
+Xvfb :0 -screen 0  1920x1080x24 &
+export DISPLAY=:0
 cp $COOKIEJAR cookies.txt
 sed -i 's/#HttpOnly_//g' cookies.txt
 sed -i 's/\t0\t/\t1999999999\t/g' cookies.txt
@@ -118,6 +120,6 @@ do
         urlpath=$(sed "s/$prefix//g"<<<$file)
         # Small risk of collision
         storepath=$(sed "s/\//_s_/g"<<<$urlpath)
-        firefox -screenshot $STORAGEDIR/$storepath-ff.png http://localhost/$url/$urlpath
+	cutycapt --delay=2000 --min-width=1920 --min-height=1080 --url=http://localhost/$url/$urlpath --out=$STORAGEDIR/$storepath-ff.png
     done
 done
