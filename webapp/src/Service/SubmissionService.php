9b4d2b7832048e459469130dc680cdcf68f89658
DOMjudge team <team@domjudge.org>
2024-04-17 08:33:02 +0200

    Amend 1f77fc0754641c1e75aed21b37e85f1e74a03e11
    Submission count should also include the ones from Jury.

--- webapp/src/Service/SubmissionService.php
+++ webapp/src/Service/SubmissionService.php
@@ -283,12 +283,10 @@ class SubmissionService
             ->andWhere($countQueryExtras['queued'])
             ->getQuery()
             ->getSingleScalarResult();
-        $counts['public'] = (clone $queryBuilder)
+        $counts['inContest'] = (clone $queryBuilder)
             ->select('COUNT(s.submitid)')
             ->join('s.contest', 'c')
-            ->join('t.category', 'cat')
             ->andWhere("s.submittime BETWEEN c.starttime AND c.endtime")
-            ->andWhere("cat.visible = 1")
             ->getQuery()
             ->getSingleScalarResult();
 
