4312438be978b2e7226e9ec551e8df2695d68a4b
Tobias Werth <werth@cs.fau.de>
2025-11-30 11:48:28 +0100

    fix

--- .github/jobs/configure-checks/setup_configure_image.sh
+++ .github/jobs/configure-checks/setup_configure_image.sh
@@ -24,5 +24,4 @@ cp submit/assert.bash .github/jobs/configure-checks/
 
 # Run the configure tests for this usecase
 mkdir /tmp/bats_logs
-echo "DOMjudge is the best" >> /tmp/bats_logs/testfile
-test_path="/__w/domjudge/domjudge" bats -o /tmp/bats_logs .github/jobs/configure-checks/all.bats
+test_path="/__w/domjudge/domjudge" bats --print-output-on-failure --gather-test-outputs-in /tmp/bats_logs .github/jobs/configure-checks/all.bats
